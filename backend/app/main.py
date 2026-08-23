4aa65ee9e059f1863d692cc80b62e3e3a439804b
HarshThakur27 <harsh273003@gmail.com>
2026-08-23 22:35:08 +0530

    Add Vercel URL to CORS allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,8 +15,10 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "https://veronica-ai-three.vercel.app"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 # @app.post("/chat")
